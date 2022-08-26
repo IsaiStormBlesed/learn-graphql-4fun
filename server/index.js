209b1d1b2c5c98b92edd0a580e6ed946d64f194d
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-26 14:41:56 +0000

    graphql init

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema.js");
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
---
+++ server/schema/schema.js
@@ -0,0 +1,1 @@
+const { buildSchema } = require("graphql");
