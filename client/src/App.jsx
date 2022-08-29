3caaa3c25c76af6e8a590b30dc177cfe54ce2192
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-29 17:03:39 +0000

    apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Header from "./components/Header";
+
+const apolloClient = new ApolloClient({
+  uri: "https://5000-isaistormbl-learngraphq-5l1rc88t499.ws-us63.gitpod.io/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <h1>HELLO</h1>
+      <ApolloProvider client={apolloClient}>
+        <Header />
+        <h1>HELLO</h1>
+      </ApolloProvider>
     </>
   );
 }
